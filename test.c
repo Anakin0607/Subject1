f1b2439ca161d4892fd775e2c7b201b9d4027687
Anakin0607 <1834645831@qq.com>
2024-04-30 16:43:47 +0800

    update file

--- test.c
+++ test.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
-int Binary_Search(int* a, int lo, int hi, int k){//在a中[lo, hi)范围内查找小于k的最大值
-    if(hi - lo < 2) return lo;//只有一个值，直接返回
-    int mid = (lo + hi)/2;
-    if(a[mid] < k){
-        return Binary_Search(a, mid, hi, k);
+int Binary_Search_it(int* a, int lo, int hi, int k){//在[lo, hi)中查找小于k的最大值
+    while(hi - lo > 1){
+        int mid = (lo + hi) / 2;
+        if(a[mid] < k){
+            lo = mid;
+        }
+        else{
+            hi = mid;
+        }
     }
-    else{
-        return Binary_Search(a, lo, mid, k);
-    }
+    return lo;
 }
 int main(){
     int* a = (int*)malloc(sizeof(int)*100);
@@ -17,6 +19,6 @@ int main(){
     for(int i = 0; i < n; ++i){
         scanf("%d", &a[i]);
     }
-    printf("%d", a[Binary_Search(a, 0, n, k)]);
+    printf("%d", a[Binary_Search_it(a, 0, n, k)]);
     return 0;
 }
